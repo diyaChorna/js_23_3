07184d82ff604e97125ed1e76de4e1d6cb3faefb
unknown <diana.chorna@gmail.com>
2021-06-01 19:36:22 +0300

    a

---
+++ main.js
@@ -0,0 +1,13 @@
+let sel = x =>document.querySelector(x);
+
+$(function(){
+      $('#one').draggable();
+      $('#two').draggable();
+      $('#three').draggable();
+      $('#four').draggable();
+      $('#five').draggable();
+      $('#six').draggable();
+      $('#seven').draggable();
+      $('#eight').draggable();
+      $('#nine').draggable()
+})
